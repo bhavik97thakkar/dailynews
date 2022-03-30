d7c5d8d6cac67fc46ba6333c9ee4f4ea82cb10c6
bhavik97thakkar <45190240+bhavik97thakkar@users.noreply.github.com>
2022-03-30 17:05:33 +0530

    fourth commit refactoring news component and adding news image, url, title, desc, page size, author, date through props and adding router to navigate between categories of news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description,imgUrl,newsUrl} =this.props;
+        let { title, description, imgUrl, newsUrl, author, date,source } = this.props;
         return (
             <div>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imgUrl?imgUrl:"https://www.tibs.org.tw/images/default.jpg"} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{title} ...</h5>
-                            <p className="card-text">{description} ...</p>
-                            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-                        </div>
+                <div className="card">
+                    <img src={imgUrl ? imgUrl : "https://www.tibs.org.tw/images/default.jpg"} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title} ... </h5>
+                        <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>
+                            {source}
+                        </span>
+                        <p className="card-text">{description} ...</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
                 </div>
             </div>
         )
---
+++ src/components/searchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import React, { Component } from 'react'
+
+export  class searchBar extends Component {
+    
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
